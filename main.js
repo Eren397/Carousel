c6e254c4c76f182743fd43962649135a7c5380e5
Anderson <andersonwhiteguitar@gmail.com>
2021-02-12 00:13:42 -0300

    inserçao do touchstart

--- main.js
+++ main.js
@@ -1,82 +1,98 @@
 const carouselSlider = document.querySelector('.carousel__slider')
-const carouselImages = document.querySelectorAll('.carousel__images')
+const carouselImages = document.querySelectorAll('.carousel__image')
 const btnNext = document.querySelector('.carousel__controls--next')
 const btnPrevious = document.querySelector('.carousel__controls--previous')
 const userEvents = ['click', 'touchstart']
+
 let index = 1
 
-const addEventListenerObj = {
-    listener(userEvents, eventObj, callback){
+const forEachObj = {
+    insertEvents(userEvents, element, callback){
         userEvents.forEach(userEvent => {
-            eventObj.addEventListener(userEvent, callback)
+            element.addEventListener(userEvent, callback)
         })
-    }
+    },
+    removeEvents(userEvents, element, callback){
+        userEvents.forEach(userEvent => {
+            element.removeEventListener(userEvent, callback)
+        })
+    }    
 }
+
+forEachObj['insertEvents'](userEvents, btnNext, nextSlide)
+forEachObj['insertEvents'](userEvents, btnPrevious, previousSlide)
+
 function cloneImages(){
     const firstImage = carouselImages[0]
     const cloneFirstImage = firstImage.cloneNode(true)
+    carouselSlider.appendChild(cloneFirstImage)    
+
     const lastImage = carouselImages[carouselImages.length - 1]
-    const cloneLastImage = lastImage.cloneNode(true)  
-    carouselSlider.appendChild(cloneFirstImage)   
-    carouselSlider.insertBefore(cloneLastImage, firstImage)    
+    const cloneLastImage = lastImage.cloneNode(true)
+    carouselSlider.insertBefore(cloneLastImage, firstImage)
 }
 cloneImages()
 
-addEventListenerObj['listener'](userEvents, btnNext, nextSlide)
-addEventListenerObj['listener'](userEvents, btnPrevious, previousSlide)
-
-function nextSlide(){
-    index++
-    carouselSlider.style.transition = `left .6s ease`
-    carouselSlider.style.left = `${-100 * index}%`   
-    this.style.display = 'none'   
-}
-function previousSlide(){
-    index--
-    carouselSlider.style.transition = `left .6s ease`
+function nextSlide(e){
+    e.preventDefault()
+    index++     
+    carouselSlider.style.transition = 'left .3s ease-in-out'
     carouselSlider.style.left = `${-100 * index}%` 
-    this.style.display = 'none'   
+    forEachObj['removeEvents'](userEvents, this, nextSlide)      
 }
+
+function previousSlide(e){
+    e.preventDefault()
+    index--
+    carouselSlider.style.transition = 'left .3s ease-in-out'
+    carouselSlider.style.left = `${-100 * index}%` 
+    forEachObj['removeEvents'](userEvents, this, previousSlide)   
+}
+
 function createIndicators(){
     const carouselIndicators = document.querySelector('.carousel__indicators')
     for(let i = 0; i < carouselImages.length; i++){
-        const indicator = document.createElement('span')
-        indicator.classList.add('carousel__indicator')
-        carouselIndicators.appendChild(indicator)        
+        const span = document.createElement('span')
+        span.classList.add('carousel__indicator')
+        carouselIndicators.appendChild(span)
     }
-    const allIndicators = document.querySelectorAll('.carousel__indicator')
-    allIndicators.forEach((all, position) => {
-        addEventListenerObj['listener'](userEvents, all, () => {
-            goToTargetImg(position)
+    const allCarouselIndicators = document.querySelectorAll('.carousel__indicator')
+    allCarouselIndicators.forEach((ind, position) => {
+        forEachObj['insertEvents'](userEvents, ind, (e) => {
+            e.preventDefault()
+            setImage(position)             
         })
     })
-    function goToTargetImg(position){
+
+    function setImage(position){
         index = position + 1
-        carouselSlider.style.transition = `left .6s ease`
-        carouselSlider.style.left = `${-100 * index}%`            
-        whereIsYouSlide()
-    }    
+        carouselSlider.style.transition = 'left .3s ease-in-out'
+        carouselSlider.style.left = `${-100 * index}%`        
+    }
+    whereIsTheImage()
 }
 createIndicators()
-function whereIsYouSlide(){
-    const allIndicators = document.querySelectorAll('.carousel__indicator')
-    allIndicators.forEach(all => {
-        all.classList.remove('carousel__indicator--red')
+function whereIsTheImage(){
+    const allCarouselIndicators = document.querySelectorAll('.carousel__indicator')
+    allCarouselIndicators.forEach(i => {
+        i.classList.remove('carousel__indicator--red')
     })
-    allIndicators[index - 1].classList.add('carousel__indicator--red')    
+    allCarouselIndicators[index - 1].classList.add('carousel__indicator--red')   
 }
-whereIsYouSlide()
 carouselSlider.addEventListener('transitionend', () => {
-    btnNext.style.display = null
-    btnPrevious.style.display = null
+
+    forEachObj['insertEvents'](userEvents, btnNext, nextSlide)
+    forEachObj['insertEvents'](userEvents, btnPrevious, previousSlide)
+
     if(index > carouselImages.length){
         index = 1
         carouselSlider.style.transition = null
         carouselSlider.style.left = `${-100 * index}%` 
-    }else if(index < 1 ){
+    }else if(index < 1){
         index = carouselImages.length
         carouselSlider.style.transition = null
-        carouselSlider.style.left = `${-100 * index}%` 
+        carouselSlider.style.left = `${-100 * index}%`         
     }
-    whereIsYouSlide()
+    whereIsTheImage()
 })
+
